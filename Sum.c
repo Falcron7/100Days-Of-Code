10a3c61808663cda3e812c18f62d4d7fc365d0b9
Falcron7 <gandhiraghv04@gmail.com>
2026-08-05 12:31:40 +0530

    Add Day 1.c

---
+++ Sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int a, b, c;
+  printf("Enter a");
+  scanf("%d", &a);
+  
+  printf("Enter b");
+  scanf("%d", &b);
+
+  printf("Enter c");
+  scanf("%d", &c);
+
+  printf("Sum is: %d", a + b + c);
+  return 0;
+}
